795dee29f6b99b71f3be15bcf418db104902ba0d
wyka0 <wyka0@users.noreply.github.com>
2026-05-31 00:27:02 +0530

    Set up Foundry project with tests, CI, and MIT license
    - Add foundry.toml (src=contracts, out=out, libs=lib, test=test)
    - Install forge-std and openzeppelin-contracts via forge install
    - Write 16 passing tests for PharosGovernor + GovernanceToken
    - Add GitHub Actions CI workflow (forge install + forge test -v)
    - Add MIT LICENSE file

--- test/PharosGovernor.t.sol
+++ test/PharosGovernor.t.sol
@@ -13,9 +13,9 @@ contract PharosGovernorTest is Test {
         governor = new PharosGovernor(
             IVotes(address(token)),
             "PharosGovernor",
-            1,       // votingDelay
-            7200,    // votingPeriod
-            100_000e18, // proposalThreshold
+            1,           // votingDelay
+            7200,        // votingPeriod
+            100_000e18,  // proposalThreshold
             40_000_000e18 // quorum
         );
     }
@@ -29,18 +29,102 @@ contract PharosGovernorTest is Test {
     }
 
     function test_votingDelay() public view {
-        assertGt(governor.votingDelay(), 0);
+        assertEq(governor.votingDelay(), 1);
     }
 
     function test_votingPeriod() public view {
-        assertGt(governor.votingPeriod(), 0);
-    }
-
-    function test_quorumNumerator() public view {
-        assertGe(governor.quorum(0), 1);
+        assertEq(governor.votingPeriod(), 7200);
     }
 
     function test_proposalThreshold() public view {
-        assertGe(governor.proposalThreshold(), 0);
+        assertEq(governor.proposalThreshold(), 100_000e18);
+    }
+
+    function test_quorum() public view {
+        assertEq(governor.quorum(0), 40_000_000e18);
+    }
+
+    function test_totalSupply() public view {
+        assertEq(token.totalSupply(), 1_000_000_000e18);
+    }
+
+    function test_tokenNameSymbol() public view {
+        assertEq(token.name(), "Pharos DAO Token");
+        assertEq(token.symbol(), "pDAO");
+    }
+
+    function test_governorToken() public view {
+        assertEq(address(governor.token()), address(token));
+    }
+
+    function _delegateAndAdvance() internal {
+        token.delegate(address(this));
+        vm.roll(block.number + 1);
+    }
+
+    function test_createProposal() public {
+        _delegateAndAdvance();
+
+        address[] memory targets = new address[](1);
+        targets[0] = address(this);
+
+        uint256[] memory values = new uint256[](1);
+        values[0] = 0;
+
+        bytes[] memory calldatas = new bytes[](1);
+        calldatas[0] = abi.encodeWithSignature("testProposal()");
+
+        uint256 proposalId = governor.propose(targets, values, calldatas, "Test proposal");
+        assertTrue(proposalId != 0);
+    }
+
+    function test_proposalStateAfterCreate() public {
+        _delegateAndAdvance();
+
+        address[] memory targets = new address[](1);
+        targets[0] = address(this);
+
+        uint256[] memory values = new uint256[](1);
+        values[0] = 0;
+
+        bytes[] memory calldatas = new bytes[](1);
+        calldatas[0] = hex"00";
+
+        uint256 proposalId = governor.propose(targets, values, calldatas, "State test");
+        assertEq(uint8(governor.state(proposalId)), uint8(IGovernor.ProposalState.Pending));
+    }
+
+    function test_delegationGrantsVotingPower() public {
+        assertEq(token.getVotes(address(this)), 0);
+        token.delegate(address(this));
+        assertEq(token.getVotes(address(this)), 1_000_000_000e18);
+    }
+
+    function test_proposalFailsWithoutDelegation() public {
+        address[] memory targets = new address[](1);
+        targets[0] = address(this);
+
+        uint256[] memory values = new uint256[](1);
+        values[0] = 0;
+
+        bytes[] memory calldatas = new bytes[](1);
+        calldatas[0] = hex"00";
+
+        vm.expectRevert();
+        governor.propose(targets, values, calldatas, "Should fail");
+    }
+
+    function test_proposalThresholdCheck() public {
+        _delegateAndAdvance();
+        assertGe(token.getVotes(address(this)), governor.proposalThreshold());
+    }
+
+    function test_quorumRequirement() public {
+        _delegateAndAdvance();
+        assertGe(token.getVotes(address(this)), governor.quorum(block.number));
+    }
+
+    function test_governorVersion() public view {
+        assertEq(governor.version(), "1");
     }
 }
